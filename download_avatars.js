dcf87f2603615809177feefab0bf17f88e38b490
tinanwlin <tinanwlin@gmail.com>
2018-02-20 03:59:47 +0000

    Print each avatar.url.

--- download_avatars.js
+++ download_avatars.js
@@ -11,16 +11,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
           'User-Agent': 'request',
           'Authorization': 'token: ' + token.GITHUB_TOKEN
       }
+
+
     };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  for(var i = 0; i < result.length; i++){
+      console.log(result[i].avatar_url);
+  }
 });
 
 
