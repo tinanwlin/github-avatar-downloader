21f1b037391cb8fd34f6a8d27e55d25d14bd2359
tinanwlin <tinanwlin@gmail.com>
2018-02-20 02:56:59 +0000

    Create a getRepoContributors function.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require ('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+
+// cd (err, ) 
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
