5d5ba77906f6d47bcd397e3f4c89a7ad45d1d750
tinanwlin <tinanwlin@gmail.com>
2018-02-20 03:44:22 +0000

    Revised the Authorization value.

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,21 @@
-var request = require ('request');
+var request = require('request');
+var token = require('./secrets');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+          'User-Agent': 'request',
+          'Authorization': 'token: ' + token.GITHUB_TOKEN
+      }
+    };
 
-// cd (err, ) 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
